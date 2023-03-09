9d9604171023628b3d66a9d5f9f13509430b3535
Luis F. Ruiz <65567581+LuisRuiz05@users.noreply.github.com>
2023-03-09 07:37:22 -0600

    Update Practica7.cu

--- Parcial2/Practica7.cu
+++ Parcial2/Practica7.cu
@@ -3,7 +3,7 @@
 #include <random>
 #include <stdio.h>
 #include <iostream>
-#include <ctime>
+#include <time.h>
 
 #define GPUErrorAssertion(ans) {gpuAssert((ans), __FILE__, __LINE__);};
 
@@ -15,6 +15,8 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 }
 
 __global__ void trans(int* a, int* b, int n);
+__global__ void conv(int* a, int* b, int n);
+
 
 int main()
 {
@@ -51,10 +53,13 @@ int main()
     cudaMemcpy(devA, a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(devB, b, size, cudaMemcpyHostToDevice);
 
-    // Solve Operation
+    // Declare grid
     dim3 block(32, 32);
+    dim3 grid(32 / (n * n), 32 / (n * n));
 
+    // Solve operations
     trans << <1, block >> > (devA, devB, n);
+    // conv << <1, block >> > (devA, devB, n);
     cudaMemcpy(b, devB, n * n * sizeof(int), cudaMemcpyDeviceToHost);
 
     // Print solution
@@ -77,14 +82,30 @@ int main()
 }
 
 __global__ void trans(int* a, int* b, int n) {
+    __shared__ int s[64];
     int tid = threadIdx.x + threadIdx.y * blockDim.x;
     int offsetBlock = blockIdx.x * blockDim.x * blockDim.y;
     int offsetRow = blockIdx.y * blockDim.x * blockDim.y * gridDim.x;
     int gid = tid + offsetBlock + offsetRow;
 
-    int row = gid/n;
-    int col = gid-row*n;
-    if (gid < n*n) {
-        b[col*n+row] = a[row*n+col];
+
+    int row = gid / n;
+    int col = gid - row * n;
+    if (gid < n * n) {
+        s[row * n + col] = a[row * n + col];
+        __syncthreads();
+        b[col * n + row] = s[row * n + col];
     }
+}
+
+__global__ void conv(int* a, int* b, int n) {
+    __shared__ int s[64];
+    int tid = threadIdx.x + threadIdx.y * blockDim.x;
+    int offsetBlock = blockIdx.x * blockDim.x * blockDim.y;
+    int offsetRow = blockIdx.y * blockDim.x * blockDim.y * gridDim.x;
+    int gid = tid + offsetBlock + offsetRow;
+
+
+    int row = gid / n;
+    int col = gid - row * n;
 }
