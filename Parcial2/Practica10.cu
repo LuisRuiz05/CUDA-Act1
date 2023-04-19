3b3d8e57a6af3cd0675f9c5a1802ee138f485da6
Luis F. Ruiz <65567581+LuisRuiz05@users.noreply.github.com>
2023-04-18 22:09:05 -0600

    Update Practica10.cu

--- Parcial2/Practica10.cu
+++ Parcial2/Practica10.cu
@@ -37,10 +37,13 @@ int main()
     int arrSize = sizeof(despStruct);
 
     despStruct* data, * ans;
+    myStruct* dataAos, * ansAos;
 
     // Allocate space for local variables
     data = (despStruct*)malloc(sizeof(despStruct));
+    dataAos = (myStruct*)malloc(sizeof(myStruct));
     ans = (despStruct*)malloc(sizeof(despStruct));
+    ansAos = (myStruct*)malloc(sizeof(myStruct));
 
     // Set data
     for (int i = 0; i < despSize; i++) {
@@ -48,29 +51,43 @@ int main()
         data-> y[i] = 2;
     }
 
+    for (int i = 0; i < despSize; i++) {
+        dataAos[i].x = 1;
+        dataAos[i].y = 2;
+    }
+
     // Print data
+    printf("Soa\n");
     for (int i = 0; i < despSize; i++) {
         printf("x: %d y: %d\n", data-> x[i], data-> y[i]);
     }
+    printf("\n");
 
-    // Print separation line
+    printf("Aos\n");
+    for (int i = 0; i < despSize; i++) {
+        printf("x: %d y: %d\n", dataAos[i].x, dataAos[i].y);
+    }
     printf("\n");
 
     despStruct* devData, * devAns;
+    myStruct* devAosData, * devAosAns;
 
     // Allocate Memory
     cudaMalloc(&devData, sizeof(despStruct));
     cudaMalloc(&devAns, sizeof(despStruct));
 
+    cudaMalloc(&devAosData, sizeof(myStruct));
+    cudaMalloc(&devAosAns, sizeof(myStruct));
+
     // Copy to GPU
     cudaMemcpy(devData, data, sizeof(despStruct), cudaMemcpyHostToDevice);
+    cudaMemcpy(devAosData, dataAos, sizeof(myStruct), cudaMemcpyHostToDevice);
 
     // Declare grid
     dim3 block(32);
     dim3 grid((despSize + 32 - 1) / (block.x));
 
-
-    // Solve operations
+    // Solve operations SOA
     soa << <grid, block >> > (devData, devAns, despSize);
     cudaMemcpy(ans, devAns, sizeof(despStruct), cudaMemcpyDeviceToHost);
 
@@ -83,10 +100,26 @@ int main()
 
     // Clean
     cudaDeviceSynchronize();
-    cudaDeviceReset();
 
     cudaFree(devData);
     cudaFree(devAns);
+
+    // Solve operations AOS
+    aos << <grid, block >> > (devAosData, devAosAns, despSize);
+    cudaMemcpy(ansAos, devAosAns, sizeof(myStruct), cudaMemcpyDeviceToHost);
+
+    // Print solution
+    printf("AOS:\n");
+    for (int i = 0; i < despSize; i++) {
+        printf("x: %d y: %d\n", ansAos[i].x, ansAos[i].y);
+    }
+
+    // Clean
+    cudaDeviceSynchronize();
+    cudaDeviceReset();
+
+    cudaFree(devAosData);
+    cudaFree(devAosAns);
 
     return 0;
 }
@@ -95,8 +128,8 @@ __global__ void aos(myStruct* data, myStruct* ans, const int size) {
     int gid = blockIdx.x * blockDim.x + threadIdx.x;
     if (gid < size) {
         myStruct temp = data[gid];
-        temp.x += 5;
-        temp.y += 10;
+        temp.x += 4;
+        temp.y += 3;
         ans[gid] = temp;
     }
 }
